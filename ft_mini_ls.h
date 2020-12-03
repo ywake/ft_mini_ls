3e94a0ac3e02e8686202454c133fc5ee6f2613ed
ywake <ywake@student.42tokyo.jp>
2020-12-04 03:54:55 +0900

    Fix: for Norm

--- ft_mini_ls.h
+++ ft_mini_ls.h
@@ -1,6 +1,8 @@
 #ifndef FT_MINI_LS_H
 # define FT_MINI_LS_H
 
-# define ERRMSG_CMDARG "Usage: ft_minils\n\t(Command line arguments are not accepted.)"
+# define ERRMSG_CMDARG "\
+Usage: ft_mini_ls\n\
+\t(Command line arguments are not accepted.)"
 
 #endif
