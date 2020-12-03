86eef708a905995524c631704b7da391b579286f
ywake <ywake@student.42tokyo.jp>
2020-12-04 07:23:50 +0900

    #3 Reproduction: ls -1r

--- ft_mini_ls.c
+++ ft_mini_ls.c
@@ -6,7 +6,7 @@
 /*   By: ywake <ywake@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/03 01:55:52 by ywake             #+#    #+#             */
-/*   Updated: 2020/12/04 04:18:11 by ywake            ###   ########.fr       */
+/*   Updated: 2020/12/04 07:17:56 by ywake            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include <errno.h>
 #include "libft.h"
 #include "ft_mini_ls.h"
+#include "compare.h"
 
 #ifdef TEST
 # include "test.h"
@@ -33,7 +34,8 @@ void	print_filename(void *content)
 {
 	struct dirent *dirent;
 	dirent = (struct dirent *)content;
-	ft_putendl_fd(dirent->d_name, STDOUT_FILENO);
+	if (dirent->d_name[0] != '.')
+		ft_putendl_fd(dirent->d_name, STDOUT_FILENO);
 }
 
 int		main(int argc, char *argv[])
@@ -53,6 +55,8 @@ int		main(int argc, char *argv[])
 		ft_lstadd_back(&list, ft_lstnew(dirent));
 	if (errno || closedir(dirp))
 		return (error(""));
+	list = ft_lst_sort(list, compare_name);
+	ft_lst_reverse(&list);
 	ft_lstiter(list, print_filename);
 	return (0);
 }
