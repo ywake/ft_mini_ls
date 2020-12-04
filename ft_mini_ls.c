32383671ed86ef018e7b586bce33537b9289c369
ywake <ywake@student.42tokyo.jp>
2020-12-04 09:01:41 +0900

    Fix: leak care.

--- ft_mini_ls.c
+++ ft_mini_ls.c
@@ -6,7 +6,7 @@
 /*   By: ywake <ywake@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/03 01:55:52 by ywake             #+#    #+#             */
-/*   Updated: 2020/12/04 08:44:43 by ywake            ###   ########.fr       */
+/*   Updated: 2020/12/04 08:58:04 by ywake            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,28 +40,6 @@ void	print_filename(void *content)
 		ft_putendl_fd(fi->dirent->d_name, STDOUT_FILENO);
 }
 
-int		get_dirinfo(char *filepath, t_list **list)
-{
-	DIR				*dirp;
-	struct dirent	*dirent;
-	t_fileinfo		*finfo;
-
-	if ((dirp = opendir(".")) == NULL)
-		return (-1);
-	while ((dirent = readdir(dirp)))
-	{
-		finfo = (t_fileinfo *)malloc(sizeof(t_fileinfo));
-		finfo->dirent = dirent;
-		finfo->stat = (struct stat *)malloc(sizeof(struct stat));
-		if (lstat(dirent->d_name, finfo->stat))
-			return (-1);
-		ft_lstadd_back(list, ft_lstnew(finfo));
-	}
-	if (errno || closedir(dirp))
-		return (-1);
-	return (0);
-}
-
 void	del_list(void *content)
 {
 	t_fileinfo *fi;
@@ -69,6 +47,32 @@ void	del_list(void *content)
 	fi = (t_fileinfo *)content;
 	free(fi->stat);
 	free(fi);
+}
+
+int		get_dirinfo(char *filepath, t_list **list)
+{
+	DIR				*dirp;
+	struct dirent	*dirent;
+	t_fileinfo		*finfo;
+
+	if ((dirp = opendir(".")))
+		return (-1);
+	while ((dirent = readdir(dirp)))
+	{
+		finfo = (t_fileinfo *)malloc(sizeof(t_fileinfo));
+		finfo->dirent = dirent;
+		finfo->stat = (struct stat *)malloc(sizeof(struct stat));
+		ft_lstadd_back(list, ft_lstnew(finfo));
+		if (lstat(dirent->d_name, finfo->stat))
+			break;
+	}
+	if (errno || closedir(dirp))
+	{
+		ft_lstclear(list, del_list);
+		closedir(dirp);
+		return (-1);
+	}
+	return (0);
 }
 
 int		main(int argc, char *argv[])
@@ -81,7 +85,7 @@ int		main(int argc, char *argv[])
 	errno = 0;
 	list = NULL;
 	if (get_dirinfo(".", &list))
-		return (error(""));
+		return (error("Error"));
 	list = ft_lst_sort(list, compare_name);
 	list = ft_lst_sort(list, compare_modtime_nsec);
 	list = ft_lst_sort(list, compare_modtime_sec);
